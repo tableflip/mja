78880bbdda41b19e7c0ac08b5bc49aec8528413e
Liam Wooding <liamwooding@gmail.com>
2014-07-15 18:16:58 +0100

    Homepage update images are now resized and saved at 200px and 800px width on upload

--- models/homepageupdate.js
+++ models/homepageupdate.js
@@ -1,5 +1,8 @@
-var keystone = require('keystone'),
-  Types = keystone.Field.Types;
+var keystone = require('keystone')
+var gm = require('gm')
+var path = require('path')
+var async = require('async')
+var Types = keystone.Field.Types
 
 var HomepageUpdate = new keystone.List('HomepageUpdate',
   { autokey: { path: 'slug', from: 'title', unique: true } }
@@ -29,7 +32,8 @@ HomepageUpdate.add({
   },
   image: {
     type: Types.LocalFile,
-    dest: __dirname + '/../public/images/homepageupdates/'
+    dest: __dirname + '/../public/images/homepageupdates/original',
+    post: { move: resizeImage }
   },
   content: {
     type: Types.Html,
@@ -38,6 +42,44 @@ HomepageUpdate.add({
     initial: true,
     required: true
   }
-});
+})
 
 HomepageUpdate.register();
+
+function resizeImage (update, request, fileData, next) {
+  var srcPath = path.join(fileData.path, fileData.filename)
+  var thumbDestPath = path.join(fileData.path, '../thumb/', fileData.filename)
+  var largeDestPath = path.join(fileData.path, '../large/', fileData.filename)
+
+  console.log(thumbDestPath, largeDestPath)
+
+  async.parallel([
+    function (cb) {
+      gm(srcPath)
+      .resize(800)
+      .write(largeDestPath, function (err) {
+        if (err) {
+          console.error(err)
+          return next()
+        }
+        cb(null)
+      })
+    },
+
+    function (cb) {
+      gm(srcPath)
+      .resize(200)
+      .crop(200, 150)
+      .write(thumbDestPath, function (err) {
+        if (err) {
+          console.error(err)
+          return next()
+        } cb(null)
+      })
+    }
+  ], function (err, cb) {
+    if (err) console.error(err)
+    console.log('done')
+    next()
+  })
+}
