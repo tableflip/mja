54039342f9225da9dbb6be17cf1a5b9f8be40be3
Liam Wooding <liamwooding@gmail.com>
2014-08-26 10:16:41 +0100

    Zoomed and enhanced logging

--- keystone.js
+++ keystone.js
@@ -25,7 +25,8 @@ keystone.init({
 	'session': true,
 	'auth': true,
 	'user model': 'User',
-	'cookie secret': 'g<?`X+s/>c{Hq6Gl2Si{If:;"m!&q(3xIoIr/oklkCP;GV6:G}(5}5I=!>0Wt^#R'
+	'cookie secret': 'g<?`X+s/>c{Hq6Gl2Si{If:;"m!&q(3xIoIr/oklkCP;GV6:G}(5}5I=!>0Wt^#R',
+  'logger': 'default'
 	
 });
 
