97f912d778164c05ff1bba138a883a1f0d40dfd4
Liam Wooding <liamwooding@gmail.com>
2014-10-07 11:19:50 +0100

    Made homepage fixed size

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -21,22 +21,4 @@ jQuery(document).ready(function ($) {
     Galleria.loadTheme('/js/lib/galleria/themes/classic/galleria.classic.min.js')
     Galleria.run('.galleria')
   }
-
-  if (!$('body').hasClass('gallery') && $(window).width() > 2000) {
-    $('.col-left').width($('body').width() * 0.30)
-  }
-
-  if (!$('body').hasClass('gallery')) {
-    if ($(window).width() > 768) {
-      $('.col-left').width($('body').width() * 0.30)
-    }
-
-    $(window).resize(function () {
-      if ($(window).width() < 768) {
-        $('.col-left').attr('style', '')
-        return
-      }
-      $('.col-left').width($('body').width() * 0.30)
-    })
-  }
 })
