e6ee42fdd3b42a37af0e43d7e74537c2f19e7b84
Liam Wooding <liamwooding@gmail.com>
2014-10-21 12:57:31 +0100

    Added pre-save hook to homepage updates to generate vimeo/youtube embeds and thumbnails

--- models/homepageupdates.js
+++ models/homepageupdates.js
@@ -1,4 +1,6 @@
 var keystone = require('keystone')
+var embedVideo = require('embed-video')
+var request = require('request')
 var Types = keystone.Field.Types
 
 var HomepageUpdate = new keystone.List('HomepageUpdate',
@@ -31,8 +33,17 @@ HomepageUpdate.add({
     type: Types.CloudinaryImage
   },
   video: {
-    label: 'Youtube/Vimeo embed code',
+    label: 'Youtube/Vimeo URL',
+    note: 'Find the video you want and copy the URL in here',
     type: String
+  },
+  videoEmbed: {
+    type: String,
+    //hidden: true
+  },
+  videoThumbnail: {
+    type: String,
+    //hidden: true
   },
   content: {
     type: Types.Html,
@@ -44,6 +55,39 @@ HomepageUpdate.add({
     wysiwyg: true,
     height: 150,
     initial: true
+  }
+})
+
+HomepageUpdate.schema.pre('save', function (next) {
+  var update = this
+  if (update.video) {
+    var vimeoRegex = /https?:\/\/(?:www\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/
+    var youtubeRegex = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/
+    var videoId
+
+    update.videoEmbed = embedVideo(update.video)
+    
+    if (update.video.match(vimeoRegex) != null) {
+      videoId = update.video.match(vimeoRegex)[2]
+      var vimeoDataUrl = 'http://vimeo.com/api/v2/video/'+ videoId +'.xml'
+      
+      request({
+        url: vimeoDataUrl,
+        json: true
+      }, function (err, message, response) {
+        if (err) console.error(err)
+        if (!response) return next()
+        update.videoThumbnail = response['thumbnail_large']
+        next()
+      })
+
+    } else if (update.video.match(youtubeRegex) != null) {
+      videoId = update.video.match(youtubeRegex)[2]
+      update.videoThumbnail = 'http://i3.ytimg.com/vi/'+ videoId +'/0.jpg'
+      next()
+    } else {
+      console.error('Failed to get a thumbnail for video: ' + update.video)
+    }
   }
 })
 
