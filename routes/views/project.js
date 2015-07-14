a980edc8fe67427cae22aba24a5ea8b522a5fa78
olizilla <oli@zilla.org.uk>
2015-07-14 14:37:04 +0100

    Add inter-project nav. Fixes #15

--- routes/views/project.js
+++ routes/views/project.js
@@ -1,23 +1,90 @@
+var async = require('async')
 var keystone = require('keystone')
 var Project = keystone.list('Project')
+var ProjectCategory = keystone.list('ProjectCategory')
 
-exports = module.exports = function(req, res) {
-  
+exports = module.exports = function (req, res) {
+
   var locals = res.locals
   var view = new keystone.View(req, res)
-  
-  locals.bodyClass = 'project'
-
   var slug = req.params.slug
 
+  async.waterfall(
+    [
+      function project (cb) {
+        findProjectBySlug(slug, cb)
+      },
+      function next (project, cb) {
+        findNextProject(project, function (err, res) {
+          cb(err, { project: project, next: res})
+        })
+      }
+    ],
+    function render (err, res) {
+      if (err) {
+        console.error(err)
+        return view.render('500')
+      }
+      locals.bodyClass = 'project'
+      locals.project = res.project
+      locals.next = res.next
+      view.render('project')
+    }
+  )
+}
+
+function findProjectBySlug (slug, cb) {
   Project.model.findOne()
-  .where({'slug': slug})
-  .populate('category')
-  .exec(function (err, project) {
-    if (err) console.error(err)
+    .where({'slug': slug})
+    .populate('category')
+    .exec(cb)
+}
 
-    locals.project = project
+function findFirstCategory (cb) {
+  ProjectCategory.model.findOne()
+    .sort('sortOrder')
+    .exec(cb)
+}
 
-    view.render('project')
-  })
+function findNextCategory (category, cb) {
+  ProjectCategory.model.findOne()
+    .where({sortOrder: { $gt: category.sortOrder }})
+    .sort('sortOrder')
+    .exec(function (err, res) {
+      if (res) {
+        cb(err, res)
+      } else {
+        findFirstCategory(cb)
+      }
+    })
+}
+
+function findFirstProject (category, cb) {
+  Project.model.findOne()
+    .where({ category: category._id })
+    .sort('sortOrder')
+    .select('name slug sortOrder category')
+    .populate('category')
+    .exec(cb)
+}
+
+function findNextProject (project, cb) {
+  Project.model.findOne()
+    .where({
+      category: project.category._id,
+      sortOrder: { $gt: project.sortOrder }
+    })
+    .sort('+sortOrder')
+    .select('name slug sortOrder category')
+    .populate('category')
+    .exec(function (err, res) {
+      if (res) {
+        cb(err, res)
+      } else {
+        findNextCategory(project.category, function (err, nextCategory) {
+          if (err) return cb(err)
+          findFirstProject(nextCategory, cb)
+        })
+      }
+    })
 }
